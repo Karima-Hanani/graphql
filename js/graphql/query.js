cf460f2535523722add8b42ded3640e841e8908f
khanani <karimahanani398@gmail.com>
2026-08-23 14:33:55 +0100

    change the query

--- js/graphql/query.js
+++ js/graphql/query.js
@@ -1,31 +1,47 @@
 export const query = `
-    {
-        user {
-            login
-            firstName
-            lastName
-            gender: attrs(path: "gender")
-            cin: attrs(path: "cin")
-            city: attrs(path: "city")
-            avatar: attrs(path:"avatarUrl")
+{
+  user {
+    login
+    firstName
+    lastName
+    gender: attrs(path: "gender")
+    cin: attrs(path: "cin")
+    city: attrs(path: "city")
+    avatar: attrs(path: "avatarUrl")
 
-            auditRatio
-            totalUp
-            totalDown
+    auditRatio
+    totalUp
+    totalDown
+    totalUpBonus
 
-            transactions(
-            where: {
-                type: { _like: "skill%" }
-            }
-            distinct_on: type
-            order_by: [
-                { type: asc }
-                { amount: desc }
-            ]
-            ) {
-            type
-            amount
-            }
-        }
+    skills: transactions(
+      where: {type: {_like: "skill%"}}
+      distinct_on: type
+      order_by: [{type: asc}, {amount: desc}]
+    ) {
+      type
+      amount
     }
+      
+    level: transactions(
+      where: {eventId: {_eq: 41}, type: {_eq: "level"}}
+      distinct_on: type
+      order_by: [{type: asc}, {amount: desc}]
+    ) {
+      type
+      amount
+    }
+
+    xp: transactions_aggregate(where:{
+      type:{_eq:"xp"} 
+      eventId:{_eq:41}
+    }){
+      aggregate {
+        sum {
+          amount
+        }
+      }
+    }
+  }
+}
 `
